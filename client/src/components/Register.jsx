0d3c6d774184f6104bb53f2f952b70aae84ab6e8
def-Rasheed <121048607+def-Rasheed@users.noreply.github.com>
2024-08-09 05:27:07 +0300

    BSPMS2426-115 BSPMS2426-106 BSPMS2426-105

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -6,6 +6,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Header from './Header';
 import CheckIcon from '@mui/icons-material/Check';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -86,9 +87,11 @@ const Register = () => {
   const handleVerification = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.get('http://localhost:3001/Verification', {params: {
-        mail:formData.mail
-      }},{ withCredentials: true });
+      const response = await axios.get('http://localhost:3001/Verification', {
+        params: {
+          mail: formData.mail
+        }
+      }, { withCredentials: true });
       console.log('Verification Response:', response.data);
       setMessages((prevMessages) => ({
         ...prevMessages,
@@ -105,159 +108,179 @@ const Register = () => {
     }
   };
 
+  const handleHome = () => {
+    console.log("Navigating to Home");
+    navigate('/Home');
+  };
+
   return (
     <div>
-      <Form onSubmit={handleSubmit} className='small-form-group'>
-      <h2 style={{ textAlign: "center" }}>Sign up</h2>
-        <Row>
-          <Col md={12}>
-            <Form.Group className="mb-3" controlId="formFname">
-              <Form.Control
-                className="small-form-group"
-                type="text"
-                placeholder="First name"
-                name="fname"
-                value={formData.fname}
-                onChange={handleChange}
-                required
-              />
-            </Form.Group>
-          </Col>
-
-          <Col md={12}>
-            <Form.Group className="mb-3" controlId="formLname">
-              <Form.Control
-                className="small-form-group"
-                type="text"
-                placeholder="Last name"
-                name="lname"
-                value={formData.lname}
-                onChange={handleChange}
-                required
-              />
-            </Form.Group>
-          </Col>
-
-          <Col md={12}>
-            <Form.Group className="mb-3" controlId="formUsername">
-              <Form.Control
-                className="small-form-group"
-                type="text"
-                placeholder="Username"
-                name="username"
-                value={formData.username}
-                onChange={handleChange}
-                required
-              />
-            </Form.Group>
-          </Col>
-
-          <Col md={12}>
-            <Form.Group className="mb-3" controlId="formEmail">
-              <Form.Control
-                className="small-form-group"
-                type="email"
-                placeholder="name@example.com"
-                name="mail"
-                value={formData.mail}
-                onChange={handleChange}
-                required
-              />
-             
-            </Form.Group>
-          </Col>
-
-          <Col md={12}>
-            <Form.Group className="small-form-group">
-              {messages.verificationMessage && <Form.Text style={{ color: 'blue' }}>{messages.verificationMessage}</Form.Text>}
-              <Form.Control
-                 className="btn btn-success"
-                type="button"
-                value={'Verify Email'}
-                onClick={handleVerification}
-              />
-            </Form.Group>
-          </Col>
-
-          <Col md={12}>
-            <Form.Group className="mb-3" controlId="formCode">
-              <Form.Control
-                className="small-form-group"
-                type="text"
-                placeholder="Enter the 4-digit code"
-                name="code"
-                value={formData.code}
-                onChange={handleChange}
-                required
-              />
-              {messages.codeError && <Form.Text style={{ color: 'red' }}>{messages.codeError}</Form.Text>}
-            </Form.Group>
-          </Col>
-
-          <Col md={12}>
-            <Form.Group className="mb-3" controlId="formPassword">
-              <Form.Control
-                className="small-form-group"
-                type="password"
-                placeholder="Password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                required
-              />
-              {messages.passwordError && <Form.Text style={{ color: 'red' }}>{messages.passwordError}</Form.Text>}
-            </Form.Group>
-          </Col>
-
-          <Col md={12}>
-            <Form.Group>
-              <Form.Label>Select your role:</Form.Label>
-              <div className="d-flex">
-                <Form.Check
-                  type="radio"
-                  id="roleDeveloper"
-                  label="Developer"
-                  name="role"
-                  value="developer"
-                  checked={formData.role === "developer"}
+      <div style={{ marginTop: "8rem" }}>
+        <button
+          className="btn btn-secondary"
+          onClick={handleHome}
+          style={{
+            position: 'absolute',
+            top: '10px',
+            left: '10px',
+            zIndex: 1000, // Ensures the button stays on top of other elements
+          }}
+        >
+          <ArrowBackIcon style={{ marginRight: '0.5rem' }} />
+          Back
+        </button>
+        <Form onSubmit={handleSubmit} className='small-form-group'>
+          <h2 style={{ textAlign: "center" }}>Sign up</h2>
+          <Row>
+            <Col md={12}>
+              <Form.Group className="mb-3" controlId="formFname">
+                <Form.Control
+                  className="small-form-group"
+                  type="text"
+                  placeholder="First name"
+                  name="fname"
+                  value={formData.fname}
                   onChange={handleChange}
-                  className="me-3"
+                  required
                 />
-                <Form.Check
-                  type="radio"
-                  id="roleManager"
-                  label="Manager"
-                  name="role"
-                  value="manager"
-                  checked={formData.role === "manager"}
-                  onChange={handleChange}
-                  className="me-3"
-                />
-                <Form.Check
-                  type="radio"
-                  id="roleTester"
-                  label="Tester"
-                  name="role"
-                  value="tester"
-                  checked={formData.role === "tester"}
-                  onChange={handleChange}
-                />
-              </div>
-            </Form.Group>
-          </Col>
+              </Form.Group>
+            </Col>
 
-          <Col md={12}>
-            <Form.Group className="mb-3 small-form-group" controlId="formSubmit">
-              <Form.Control type="submit" value={isLoading ? "Registering..." : "Register"} className="btn btn-primary" disabled={isLoading} />
-            </Form.Group>
-          </Col>
-        </Row>
-        {messages.errorMessage && <p className="error-message" style={{ color: 'red' }}>{messages.errorMessage}</p>}
-        {messages.successMessage && <p className="success-message" style={{ color: 'green' }}>{messages.successMessage}</p>}
-        <div className="text-center">
-          <a href="/login">Already have an account?</a>
-        </div>
-      </Form>
+            <Col md={12}>
+              <Form.Group className="mb-3" controlId="formLname">
+                <Form.Control
+                  className="small-form-group"
+                  type="text"
+                  placeholder="Last name"
+                  name="lname"
+                  value={formData.lname}
+                  onChange={handleChange}
+                  required
+                />
+              </Form.Group>
+            </Col>
+
+            <Col md={12}>
+              <Form.Group className="mb-3" controlId="formUsername">
+                <Form.Control
+                  className="small-form-group"
+                  type="text"
+                  placeholder="Username"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleChange}
+                  required
+                />
+              </Form.Group>
+            </Col>
+
+            <Col md={12}>
+              <Form.Group className="mb-3" controlId="formEmail">
+                <Form.Control
+                  className="small-form-group"
+                  type="email"
+                  placeholder="name@example.com"
+                  name="mail"
+                  value={formData.mail}
+                  onChange={handleChange}
+                  required
+                />
+
+              </Form.Group>
+            </Col>
+
+            <Col md={12}>
+              <Form.Group className="small-form-group">
+                {messages.verificationMessage && <Form.Text style={{ color: 'blue' }}>{messages.verificationMessage}</Form.Text>}
+                <Form.Control
+                  className="btn btn-success"
+                  type="button"
+                  value={'Verify Email'}
+                  onClick={handleVerification}
+                />
+              </Form.Group>
+            </Col>
+
+            <Col md={12}>
+              <Form.Group className="mb-3" controlId="formCode">
+                <Form.Control
+                  className="small-form-group"
+                  type="text"
+                  placeholder="Enter the 4-digit code"
+                  name="code"
+                  value={formData.code}
+                  onChange={handleChange}
+                  required
+                />
+                {messages.codeError && <Form.Text style={{ color: 'red' }}>{messages.codeError}</Form.Text>}
+              </Form.Group>
+            </Col>
+
+            <Col md={12}>
+              <Form.Group className="mb-3" controlId="formPassword">
+                <Form.Control
+                  className="small-form-group"
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                />
+                {messages.passwordError && <Form.Text style={{ color: 'red' }}>{messages.passwordError}</Form.Text>}
+              </Form.Group>
+            </Col>
+
+            <Col md={12}>
+              <Form.Group>
+                <Form.Label>Select your role:</Form.Label>
+                <div className="d-flex">
+                  <Form.Check
+                    type="radio"
+                    id="roleDeveloper"
+                    label="Developer"
+                    name="role"
+                    value="developer"
+                    checked={formData.role === "developer"}
+                    onChange={handleChange}
+                    className="me-3"
+                  />
+                  <Form.Check
+                    type="radio"
+                    id="roleManager"
+                    label="Manager"
+                    name="role"
+                    value="manager"
+                    checked={formData.role === "manager"}
+                    onChange={handleChange}
+                    className="me-3"
+                  />
+                  <Form.Check
+                    type="radio"
+                    id="roleTester"
+                    label="Tester"
+                    name="role"
+                    value="tester"
+                    checked={formData.role === "tester"}
+                    onChange={handleChange}
+                  />
+                </div>
+              </Form.Group>
+            </Col>
+
+            <Col md={12}>
+              <Form.Group className="mb-3 small-form-group" controlId="formSubmit">
+                <Form.Control type="submit" value={isLoading ? "Registering..." : "Register"} className="btn btn-primary" disabled={isLoading} />
+              </Form.Group>
+            </Col>
+          </Row>
+          {messages.errorMessage && <p className="error-message" style={{ color: 'red' }}>{messages.errorMessage}</p>}
+          {messages.successMessage && <p className="success-message" style={{ color: 'green' }}>{messages.successMessage}</p>}
+          <div className="text-center">
+            <a href="/login">Already have an account?</a>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 };
